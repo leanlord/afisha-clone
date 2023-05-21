422ba0c6b306ae7572e7ee2d00bbba322fcbe58a
Vladimir Dadyka <dadyka.vova@gmail.com>
2023-05-21 17:43:20 +0300

    fix: fix configs

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,22 @@
-const config = {
-  apiBaseURI: "http://localhost:3001",
+const index = {
+  local: {
+    apiBaseURI: "http://localhost:3001",
+  },
+  prod: {
+    apiBaseURI: "http://v1382990.hosted-by-vdsina.ru:3001",
+  },
 };
+
+let config;
+switch (process.env.VUE_APP_CONFIG) {
+  case "local":
+    config = index.local;
+    break;
+  case "prod":
+    config = index.prod;
+    break;
+  default:
+    config = index.local;
+}
 
 export default Object.freeze(config);
